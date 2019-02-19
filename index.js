700740e690addd0c42b9f2380b77ec7b08a20c80
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-20 10:10:36 +1300

    websockets demo done

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-server = app.listen(port,()=>{
+
+const server = app.listen(port,()=>{
     console.log('Server is listening on port', port)
 })
 
 app.use(express.static('public'))
 
-
 //in order to create an websockets connection i have reuired a lib call socket.io, there are other lib like ws, ws-server etc. that can achieve the same thing. I choose socket.io is simply because there are more tutoiral on it. 
 const serverSocket = require('socket.io')
 
 //first we need to set up the socket, socket here is an function, it takes one parameter which is the server. What this does is to tell socket.io, we want websocket to work on this server. When there is a client calling this server, please setup a websocket connection between the client and server.
-const io = serverSocket(server)
+const webSocket = serverSocket(server)
 
-io.on('connection', (clientSocket)=>{
-    console.log('made socket connection',clientSocket.id)
+
+//socket.id is the socket id of that unique connection. the id will keep the same during the connection and everytime you refresh the page, the socket id change, it is because when refresh, the connection disconnect and reconnect again, and whenever a new connection happedn an new socket is setup
+webSocket.on('connection', (socket)=>{
+    console.log('Setting up webSocket Connection',socket.id)
+    socket.on('chat', function(data){
+        webSocket.sockets.emit('chat',data)
     })
+
+    socket.on('typing', function(user){
+        socket.broadcast.emit('typing',user)
+    })
+})
+
 
--- public/client.js
+++ public/client.js
@@ -1,2 +1,39 @@
-//make client connection
+//Set up client connection, when client makes a connect to the server, sets up an websocket connection
 const clientSocket = io.connect('http://localhost:3000')
+
+
+//Getting Elements from DOM
+
+const message = document.getElementById('message')
+const username = document.getElementById('username')
+const sendBtn  = document.getElementById('sendBtn')
+const output = document.getElementById('output')
+const inputMonitor = document.getElementById('changeMonitor')
+const clearBtn = document.getElementById('clearBtn')
+
+
+//Emit events to server
+
+sendBtn.addEventListener('click', ()=>{
+    clientSocket.emit('chat', {
+        message: message.value,
+        username: username.value
+    })
+})
+
+message.addEventListener('keypress', ()=>{
+    clientSocket.emit('typing', username.value)
+})
+
+//Listen events from server
+
+clientSocket.on('chat', (data)=>{
+    output.innerHTML += `<p><strong> ${data.username} : &nbsp </strong><span> ${data.message} </span></p>`
+    inputMonitor.innerHTML = ""
+    message.value = ""
+})
+
+clientSocket.on('typing',(user)=>{
+    inputMonitor.innerHTML = `<p><em>${user} is typing a message...</em></p>`
+    setTimeout(()=>{inputMonitor.innerHTML = ''},1000)
+})
